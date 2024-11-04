b591739af74ead267c8ed297c97abd3c6753fd5a
Dima <josehenrikejh572@gmail.com>
2024-11-04 15:12:25 -0300

    refatorando program e endpoint

--- Dima.Api/Endpoints/Endpoint.cs
+++ Dima.Api/Endpoints/Endpoint.cs
@@ -1,7 +1,10 @@
 using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
+using Dima.Api.Endpoints.Identity;
 using Dima.Api.Endpoints.Transactions;
+using Dima.Api.Models;
 using DIma.Api.Endpoints.Categories;
+using DIma.APi.Endpoints.Identity;
 
 namespace Dima.Api.Endpoints;
 
@@ -10,6 +13,10 @@ public static class Endpoint
     public static void MapEndpoints(this WebApplication app)
     {
         var endpoints = app.MapGroup("");
+
+        endpoints.MapGroup("/")
+            .WithTags("Health Check")
+            .MapGet("/", () => new { message = "OK" });
 
         endpoints.MapGroup("v1/categories")
             .WithTags("Categories")
@@ -28,6 +35,15 @@ public static class Endpoint
             .MapEndpoint<DeleteTransactionEndpoint>()
             .MapEndpoint<GetTransactionByIdEndpoint>()
             .MapEndpoint<GetTransactionsByPeriodEndpoint>();
+
+        endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapIdentityApi<User>();
+
+        endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapEndpoint<LogoutEndpoint>()
+            .MapEndpoint<GetRolesEndpoint>();
     }
 
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app)
